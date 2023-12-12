b218426dc83927f65d93d62a3acc4b74d317cb2c
Oleg <oleg-dmitriewich@yandex.ru>
2023-12-12 22:49:08 +0200

    rewritten via Page Object Model. Added class BasePage and classes MainPage(BasePage), LoginPage(BasePage). CSS_SELECTORs and ENDPOINTs stored in locators.py

---
+++ pages/login_page.py
@@ -0,0 +1,28 @@
+from selenium.common import NoSuchElementException
+
+from .base_page import BasePage
+from .locators import LoginPageLocators, Endpoints
+
+
+class LoginPage(BasePage):
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert Endpoints.LOGIN_PAGE_ENDPOINT in self.browser.current_url, 'incorrect url address'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
