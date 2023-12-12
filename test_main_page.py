652a22e16c336fe205e813fe7942df3680020196
Oleg <oleg-dmitriewich@yandex.ru>
2023-12-12 18:47:19 +0200

    rewritten via Page Object Model. Added class BasePage and class MainPage(BasePage).

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,18 @@
 import time
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 
 # pytest -s -v --language=es
 # pytest -s -v --browser_name=firefox
 # pytest -s -v --language=es --browser_name=firefox
+# pytest -v --tb=line --language=en test_main_page.py
+
